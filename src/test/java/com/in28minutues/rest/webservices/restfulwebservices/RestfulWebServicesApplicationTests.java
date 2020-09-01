d039a3a76b77eaa76cb563d4b1d0f412fe012aab
Amit kumar <51286655+amitrepository@users.noreply.github.com>
2020-09-02 00:05:59 +0530

    Update RestfulWebServicesApplicationTests.java

--- src/test/java/com/in28minutues/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
+++ src/test/java/com/in28minutues/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -2,7 +2,7 @@ package com.in28minutues.rest.webservices.restfulwebservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class RestfulWebServicesApplicationTests {
 
