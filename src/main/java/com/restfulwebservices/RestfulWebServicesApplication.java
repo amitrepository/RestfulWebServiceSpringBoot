7569cfed715f5874f44b1d0da5873ecadbdb3fab
Amit kumar <51286655+amitrepository@users.noreply.github.com>
2020-09-02 00:03:03 +0530

    Update RestfulWebServicesApplication.java

--- src/main/java/com/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/restfulwebservices/RestfulWebServicesApplication.java
@@ -3,7 +3,7 @@ package com.restfulwebservices;
 import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Test
 //@EnableAutoConfiguration
 @SpringBootApplication
 public class RestfulWebServicesApplication  {
